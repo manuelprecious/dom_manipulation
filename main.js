2cb801ca96c063126835fa388a04ad32cf5e1a69
Manuel-Smith <riddicksmith37@gmail.com>
2022-10-18 05:00:25 +0300

    added a readme file.

---
+++ main.js
@@ -0,0 +1,4 @@
+const parentDiv = document.querySelector('#parent');
+
+// console.dir(parentDiv.children[1].previousElementSibling);
+ console.dir(parentDiv.childNodes)
---
+++ readme.md
@@ -0,0 +1,9 @@
+DOM Events to master this week
+
+User Interface Events
+Focus events
+Mouse Events
+Wheel Events
+Input Events
+Keyboard events
+composition events
